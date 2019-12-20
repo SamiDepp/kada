de1a3c28bab0cc505b1cfd2d731bc6aaa026a398
s-deb <s-deb@enageo.com>
2019-12-21 00:38:59 +0100

    image to add

--- src/main/java/com/autoecole/kada/repository/ExamenRepository.java
+++ src/main/java/com/autoecole/kada/repository/ExamenRepository.java
@@ -3,10 +3,11 @@ package com.autoecole.kada.repository;
 
 import com.autoecole.kada.entity.Examen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "examens", path = "examens")
 public interface ExamenRepository extends JpaRepository<Examen, Integer> {
 
 }
